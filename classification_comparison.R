82cd6a33aeb22ec983c0743a689a6ee1a91039c3
Noah Dean <Noah715@gmail.com>
2020-06-16 15:32:26 -0400

    Changed my classification to match the update to the classifier

--- classification_comparison.R
+++ classification_comparison.R
@@ -6,14 +6,42 @@ library(tidyverse)
 
 path_of_code <- rprojroot::find_rstudio_root_file()
 
-noah <- read_csv('./clean_data/plot_level_with_class2.csv')
+noah <- read_csv('./clean_data/plot_level_with_class.csv')
 sal <- read_csv('./clean_data/plot_classification.csv')
 plot_level <- read_csv('./clean_data/plots.csv')
-all_data <- read_csv('./clean_data/UMRS_FPF_clean.csv')
+clean_data <- read_csv('./clean_data/UMRS_FPF_clean.csv')
 
-species <- unique(all_data$TR_SP)
+df <- clean_data
 
-plot_classifier <- function(plot){
+plot_freq <- df %>%
+  group_by(PID, TR_SP) %>%
+  summarize(Freq = n()) %>%
+  spread(TR_SP, Freq) %>%
+  replace(is.na(.), 0)
+
+plot_BA <- df %>%
+  group_by(PID, TR_SP) %>%
+  summarize(BA = sum(BasalArea)) %>%
+  spread(TR_SP, BA) %>%
+  replace(is.na(.), 0)
+
+plot_rel_BA<- df %>%
+  group_by(PID, TR_SP) %>%
+  summarize(BA = sum(BasalArea)) %>%
+  group_by(PID) %>%
+  mutate(rel_BA = BA / sum(BA)) %>%
+  select(-BA) %>%
+  spread(TR_SP, rel_BA) %>%
+  replace(is.na(.), 0)
+
+plots <- inner_join(plot_BA, plot_rel_BA, by = c("PID"), suffix = c("_BA","_rel_BA"))
+plots <- inner_join(plot_freq, plots, by = c("PID"))
+plots
+
+
+species <- unique(df$TR_SP)
+
+plot_classifier <- function(plot, print = F){
   
   # Finds percentage of total trees each species makes in the plot
   percentage <- plot[species]/sum(plot[species])
@@ -23,7 +51,7 @@ plot_classifier <- function(plot){
   s1 <- species[percentage_order[1]]
   
   # single species dominance check
-  if ((percentage[s1] > 0.8) & (plot[paste(s1, 'rel_TPA', sep = '_')] > 0.8)){
+  if ((percentage[s1] > 0.8) & (plot[paste(s1, 'rel_BA', sep = '_')] > 0.8)){
     output <- paste('D', s1, sep = '_')
     return(output)
   }
@@ -44,20 +72,22 @@ plot_classifier <- function(plot){
     # gets species 2 to check
     s2 <- species[percentage_order[i]]
     
+    if (print) print(s2)
+    
     # Checks criteria that each individual species must meet to be considered codominant
-    s1_crit <- ((percentage[s1] > 0.2) | (plot[paste(s1, 'rel_TPA', sep = '_')] > 0.2))
-    s2_crit <- ((percentage[s2] > 0.2) | (plot[paste(s2, 'rel_TPA', sep = '_')] > 0.2))
+    s1_crit <- ((percentage[s1] > 0.2) | (plot[paste(s1, 'rel_BA', sep = '_')] > 0.2))
+    s2_crit <- ((percentage[s2] > 0.2) | (plot[paste(s2, 'rel_BA', sep = '_')] > 0.2))
     
     # adds the percentage of tree and tpa that species 2 makes up
     # if either is above 20% after checking the criteria, then we break the loop
     extra_percent <- extra_percent + percentage[s2]
-    extra_TPA <- extra_TPA + plot[paste(s2, 'rel_TPA', sep = '_')]
+    extra_TPA <- extra_TPA + plot[paste(s2, 'rel_BA', sep = '_')]
     
     if (s1_crit & s2_crit){
       # If both criteria are met, then we check if the total percentage and total TPA 
       # is greater than 80%
       tot_percent <- percentage[s1] + percentage[s2]
-      tot_TPA <-plot[paste(s1, 'rel_TPA', sep = '_')] + plot[paste(s2, 'rel_TPA', sep = '_')]
+      tot_TPA <-plot[paste(s1, 'rel_BA', sep = '_')] + plot[paste(s2, 'rel_BA', sep = '_')]
       
       if ((tot_percent > 0.8) & (tot_TPA > 0.8)){
         # if both are above 80%, then we classify the forest as a codominant
@@ -89,24 +119,25 @@ noah
 
 
 
-noah[c('sp1', 'sp2', 'type')] <- noah$class %>% str_split_fixed(., '_', n = Inf)
+noah[c('type', 'sp1', 'sp2')] <- noah$class %>% str_split_fixed(., '_', n = Inf)
 
-noah$class
-
-noah[c('sp1', 'sp2', 'type')]
-
-noah_m <- noah %>% filter(sp1 == 'M') %>% 
-  replace(., 'type', 'M') %>% 
-  replace('sp1', '')
-
-noah_d <- noah %>% filter(sp2 == 'D') %>% 
-  replace('type', 'D') %>% 
-  replace('sp2', '')
-
-noah_cd <- noah %>% filter(type == 'CD')
-
-noah_fix <- bind_rows(noah_m, noah_cd, noah_d) %>% select(PID, sp1, sp2, type)
-noah_fix
+noah_fix <- noah %>% select(PID, 'sp1', 'sp2', 'type')
+# noah$class
+# 
+# noah[c('sp1', 'sp2', 'type')]
+# 
+# noah_m <- noah %>% filter(sp1 == 'M') %>% 
+#   replace(., 'type', 'M') %>% 
+#   replace('sp1', '')
+# 
+# noah_d <- noah %>% filter(sp2 == 'D') %>% 
+#   replace('type', 'D') %>% 
+#   replace('sp2', '')
+# 
+# noah_cd <- noah %>% filter(type == 'CD')
+# 
+# noah_fix <- bind_rows(noah_m, noah_cd, noah_d) %>% select(PID, sp1, sp2, type)
+# noah_fix
 
 sal[c('sp1', 'sp2')] <- sal$Type %>% str_split_fixed(.,' and ', n = Inf)
 
@@ -127,6 +158,10 @@ sal_fix <- bind_rows(sal_m, sal_d, sal_cd) %>% select(PID, sp1, sp2, type)
 sal_fix
 noah_fix
 
+nrow(sal_fix)
+nrow(noah_fix)
+nrow(plot_level)
+
 ?inner_join
 
 ##### Analysis of the classifications #####
@@ -137,28 +172,11 @@ comparison %>% filter(type_noah != type_sal)
 # This was the first one to appear on the above table
 # Noah's function classifies it as mixed
 # Sal's classifies it as codominant between ACSA2 and PODE3
-wrong_class <- plot_level %>% filter(PID == '12Mile-1-20')
+plot_level <- plots
+wrong_class <- plot_level %>% filter(PID == 'ANGLE-1-85')
 
+wrong_class[species]
 sum(wrong_class[species])
-wrong_class[paste(species, 'rel_TPA', sep = '_')]
+wrong_class[paste(species, 'rel_BA', sep = '_')]
 
-wrong_class['ACSA2']/sum(wrong_class[species])
-(wrong_class['ACSA2'] + wrong_class['PODE3'])/sum(wrong_class[species])
-
-# This is less than 80% so it should be a mixed forest
-wrong_class[paste('ACSA2', 'rel_TPA', sep = '_')] + wrong_class[paste('PODE3', 'rel_TPA', sep = '_')]
-
-
-comparison %>% filter(type_noah != type_sal)  %>%
-  filter(type_noah != 'M')
-
-# Sal classifies this as dominant with ACSA2
-# Noah classifies it as codominant between ACSA2 and ULAM
-wrong_class_2 <- plot_level %>% filter(PID == '12Mile-1-23')
-wrong_class_2
-wrong_class_2['ACSA2']/sum(wrong_class_2[species])
-
-
-wrong_class_2[paste(species, 'rel_TPA', sep = '_')]
-
-# ACSA2 makes up 84% of the trees by count but only 
+plot_classifier(wrong_class, pr = T)
