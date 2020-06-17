8a89b357ee1b231f012b70ce937b6d073503d498
Noah Dean <Noah715@gmail.com>
2020-06-17 15:24:13 -0400

    Changed classification to account for TPA and BA instead of tree abundance and BA

--- classification.R
+++ classification.R
@@ -36,10 +36,27 @@ plot_rel_BA<- df %>%
   spread(TR_SP, rel_BA) %>%
   replace(is.na(.), 0)
 
+plot_TPA <- df %>%
+  group_by(PID, TR_SP) %>%
+  summarize(TPA = sum(TreesPerAcre)) %>%
+  spread(TR_SP, TPA) %>%
+  replace(is.na(.), 0)
+
+plot_rel_TPA <- df %>%
+  group_by(PID, TR_SP) %>%
+  summarize(TPA = sum(TreesPerAcre)) %>%
+  group_by(PID) %>%
+  mutate(rel_TPA = TPA / sum(TPA)) %>%
+  select(-TPA) %>%
+  spread(TR_SP, rel_TPA) %>%
+  replace(is.na(.), 0)
+
 plots <- inner_join(plot_BA, plot_rel_BA, by = c("PID"), suffix = c("_BA","_rel_BA"))
 plots <- inner_join(plot_freq, plots, by = c("PID"))
-plots
 
+plots_tpa <- inner_join(plot_TPA, plot_rel_TPA, by = c("PID"), suffix = c("_TPA","_rel_TPA"))
+plots <- inner_join(plots_tpa, plots, by = c("PID"))
+plots
 # Adds a class column to the plot data frame
 # All entries should be NA to start
 
@@ -57,16 +74,17 @@ plot_level$class
 # coming first.
 # This function takes an individual plot as an input
 plot_classifier <- function(plot){
-  
+
   # Finds percentage of total trees each species makes in the plot
-  percentage <- plot[species]/sum(plot[species])
+  percentage <- plot[paste(species, 'rel_TPA', sep = '_')]
   percentage_order <- order(-percentage)
 
   # Finds the most abundant species
   s1 <- species[percentage_order[1]]
 
+  
   # single species dominance check
-  if ((percentage[s1] > 0.8) & (plot[paste(s1, 'rel_BA', sep = '_')] > 0.8)){
+  if ((percentage[paste(s1, 'rel_TPA', sep = '_')] > 0.8) & (plot[paste(s1, 'rel_BA', sep = '_')] > 0.8)){
     output <- paste('D', s1, sep = '_')
     return(output)
   }
@@ -88,24 +106,24 @@ plot_classifier <- function(plot){
     s2 <- species[percentage_order[i]]
   
     # Checks criteria that each individual species must meet to be considered codominant
-    s1_crit <- ((percentage[s1] >= 0.2) | (plot[paste(s1, 'rel_BA', sep = '_')] >= 0.2))
-    s2_crit <- ((percentage[s2] >= 0.2) | (plot[paste(s2, 'rel_BA', sep = '_')] >= 0.2))
+    s1_crit <- ((percentage[paste(s1, 'rel_TPA', sep = '_')] >= 0.2) | (plot[paste(s1, 'rel_BA', sep = '_')] >= 0.2))
+    s2_crit <- ((percentage[paste(s2, 'rel_TPA', sep = '_')] >= 0.2) | (plot[paste(s2, 'rel_BA', sep = '_')] >= 0.2))
     
     # adds the percentage of tree and tpa that species 2 makes up
     # if either is above 20% after checking the criteria, then we break the loop
-    extra_percent <- extra_percent + percentage[s2]
+    extra_percent <- extra_percent + percentage[paste(s2, 'rel_TPA', sep = '_')]
     extra_TPA <- extra_TPA + plot[paste(s2, 'rel_BA', sep = '_')]
     
     if (s1_crit & s2_crit){
       # If both criteria are met, then we check if the total percentage and total TPA 
       # is greater than 80%
-      tot_percent <- percentage[s1] + percentage[s2]
+      tot_percent <- percentage[paste(s1, 'rel_TPA', sep = '_')] + percentage[paste(s2, 'rel_TPA', sep = '_')]
       tot_TPA <-plot[paste(s1, 'rel_BA', sep = '_')] + plot[paste(s2, 'rel_BA', sep = '_')]
   
       if ((tot_percent > 0.8) & (tot_TPA > 0.8)){
         # if both are above 80%, then we classify the forest as a codominant
         # with the two species
-        output <- paste('CD', s1, s2, sep ='_')
+        output <- paste('CD', sort(c(s1,s2))[1], sort(c(s1,s2))[2], sep ='_')
         return(output)
       }
     }
@@ -121,6 +139,8 @@ plot_classifier <- function(plot){
   return('M')
 }
 
+plot_classifier(plot_level[1,])
+
 start <- now() # used for timing the classification algorithm
 for (i in 1:nrow(plot_level)){
   plot_level$class[i] <- plot_classifier(plot_level[i, ]) 
@@ -135,7 +155,7 @@ end <- now() # end of classification algorithm
 end - start # cam run this to see how long it took
 
 # writes a csv to the clean_data directory called plot_level_with_class.csv
-write_csv(plot_level, './clean_data/plot_level_with_class2.csv')
+write_csv(plot_level, './clean_data/plot_level_with_class3.csv')
 
 # can see all the unique classes
 unique(plot_level$class)
@@ -143,7 +163,20 @@ unique(plot_level$class)
 # sorts the classes based on count.  Can see that mixed is the most abundant
 # would probably then want to do some sort of clustering algorithm on just the 
 # mixed forests since this is such a broad category
-plot_level %>% count(class, sort = T)
+plot_level %>% ungroup() %>% 
+  count(class, sort = T)
 
+paste('CD', sort(c('v', 'a'))[1], sort(c('v', 'a'))[2], sep ='_')
 
+x <- plot_level %>% ungroup %>%
+  select(class) %>% 
+  mutate('type' = NA, 'sp1' = NA, 'sp2' = NA)
+
+x[c('type', 'sp1', 'sp2')] <- plot_level$class %>% str_split_fixed(., '_', n = Inf)
+x %>% group_by(type) %>% 
+  summarize(count = n_distinct(class))
+x
+?ungroup
+
+plot_level %>% filter(class == 'D_SNAG')
 
