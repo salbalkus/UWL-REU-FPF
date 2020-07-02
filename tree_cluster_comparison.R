014396e01cba970460fda1accc3a6a19c6ca3a74
Salvador Balkus <salbalkus@gmail.com>
2020-07-02 15:05:16 -0400

    tree cluster comparison updates

--- tree_cluster_comparison.R
+++ tree_cluster_comparison.R
@@ -14,16 +14,77 @@ dissim <- read_csv("dissimilarity_matrix.csv")
 dissim <- as.matrix(dissim)
 
 cluster_h <- hclust(as.dist(dissim), method = "ward.D2")
-plots <- left_join(read_csv("clean_data/plots_full.csv"), read_csv("clean_data/plot_classification.csv"), by = "PID")
+plots <- read_csv("clean_data/plots_nonrel.csv")
 plots_acsa2 <- plots %>% filter(Type == "ACSA2")
 plots_acsa2
-form <- paste( "cluster ~", paste0(colnames(plots_acsa2)[2:(ncol(plots_acsa2)-2)], collapse = " + "))
+
+
+obs <- read_csv("clean_data/UMRS_FPF_clean.csv")
+labels <- read_csv("clean_data/plot_classification.csv")
+obs <- left_join(obs, labels, by = "PID")
+obs_acsa2 <- obs %>% filter(Type == "ACSA2")
+
+feature_engineer <- function(obs, dom){
+  
+  obs_target <- obs %>% filter(Type == dom)
+  
+  #Feature Engineering
+  hard_mast <- c("QUPA2","QUBI","QUVE","QUMA2","QULY","CACO15","QURU","CALA21","CAOV2","QUAL")
+  food <- c("CAIL2","CEOC","JUNI","PRSE2","MAPO")
+  pioneer <- c("PODE3","SALIX")
+  shade_tolerant <- c("ACNE2","MOAL","MORUS")
+  other <- c("FRPE","ULAM","PLOC","BENI","FOAC","GLTR","DIVI5","TIAM","GYDI","ROPS","CEOC2","CRATA","FRNI","OTHER","TADI2","PIRE","CORNU")
+  
+  fe_target<- obs_target %>% group_by(PID) %>% 
+    summarize(
+      Total_TPA = sum(TreesPerAcre),
+      Total_BA = sum(BasalArea),
+      Num_Species = n_distinct(TR_SP),
+      Dom_TPA = sum(TreesPerAcre[TR_SP == dom]),
+      Dom_BA = sum(BasalArea[TR_SP == dom ]),
+      SNAG_TPA = sum(TreesPerAcre[TR_SP == "SNAG"]),
+      SNAG_BA = sum(BasalArea[TR_SP == "SNAG" ]),
+      HARDMAST_TPA = sum(TreesPerAcre[TR_SP %in% hard_mast]),
+      HARDMAST_BA = sum(BasalArea[TR_SP %in% hard_mast ]),
+      FOOD_TPA = sum(TreesPerAcre[TR_SP %in% food]),
+      FOOD_BA = sum(BasalArea[TR_SP %in% food ]),
+      PIONEER_TPA = sum(TreesPerAcre[TR_SP %in% pioneer]),
+      PIONEER_BA = sum(BasalArea[TR_SP %in% pioneer ]),
+      SHADE_TPA = sum(TreesPerAcre[TR_SP %in% shade_tolerant]),
+      SHADE_BA = sum(BasalArea[TR_SP %in% shade_tolerant ]),
+      OTHER_TPA = sum(TreesPerAcre[TR_SP %in% other]),
+      OTHER_BA = sum(BasalArea[TR_SP %in% other ])
+    )
+    
+  return(fe_target)
+}
+
+#Train the model
+
+
+form <- paste( "cluster ~", paste0(colnames(fe_acsa2)[2:(ncol(fe_acsa2)-2)], collapse = " + "))
 
 ### Using comparisons between tree and hierarchical clustering ###
-cut <- cutree(cluster_h, k = 10)
-plots_acsa2$cluster <- cut
+cut <- cutree(cluster_h, k = 4)
+fe_acsa2$cluster <- as.factor(cut)
 
-tree <- rpart(data = plots_acsa2, formula = form, method = "class", control = rpart.control(maxdepth = ceiling(log(2*10,2)), cp = 0, minbucket = 6))
+tree <- rpart(data = fe_acsa2, formula = form, method = "class", 
+              parms = list(split = "information"),
+              control = rpart.control(minbucket = 1))
+
+
+rpart.plot(tree)
+
+ggplot(fe_acsa2) + geom_point(aes(x = log(Dom_TPA), y = log(Dom_BA), color = cluster))
+
+tab <- table(fe_acsa2$cluster, predict(tree, fe_acsa2, type = "vector"))
+sum(diag(tab)) / sum(tab)
+
+#maxdepth = ceiling(log(2*10,2))
+
+
+
+#############THIS IS OLD CODE FOR TESTING V-MEASURE ON CLUSTERING ################
 
 #Homogeneity and Completeness
 # here we assume the Ward clustering to be the "true classes"
@@ -38,31 +99,44 @@ c <- 1 - condentropy(plots_acsa2$tree, plots_acsa2$cluster) / entropy(plots_acsa
 validity <- 2 * ((h*c) / (h + c))
 
 #Now we compare across multiple clusters
-max_clusters <- 10
+max_clusters <- 11
 vmeasures <- vector(length = max_clusters - 1)
+accuracies <- vector(length = max_clusters - 1)
+trees <- vector("list", length = max_clusters - 1)
+
 for(n in 2:max_clusters){
   cut <- cutree(cluster_h, k = n)
   plots_acsa2$cluster <- cut
   
-  tree <- rpart(data = plots_acsa2, formula = form, method = "class", control = rpart.control(maxdepth = ceiling(log(2*n,2)), cp = 0, minbucket = 1))
+  tree <- rpart(data = plots_acsa2, formula = form, method = "class", 
+                parms = list(split = "information"),
+                control = rpart.control(maxdepth = ceiling(log(2*10,2)),cp = 0, minbucket = 1))
+  trees[n-1] <- tree
   plots_acsa2$tree <- predict(tree, plots_acsa2, type = "vector")
   
   h <- 1 - condentropy(plots_acsa2$cluster, plots_acsa2$tree) / entropy(plots_acsa2$cluster)
   c <- 1 - condentropy(plots_acsa2$tree, plots_acsa2$cluster) / entropy(plots_acsa2$tree)
   
   vmeasures[n-1] <- 2 * ((h*c) / (h + c))
+  accuracies[n-1] <- sum(diag(table(plots_acsa2$cluster, plots_acsa2$tree))) / nrow(plots_acsa2)
 }
 #higher validity measures are better
-wrap <- data.frame(Clusters = (seq(1:9) + 1), V.measure = vmeasures)
-wrap
+wrap <- data.frame(Clusters = (seq(1:(max_clusters-1)) + 1), V.measure = vmeasures, Accuracy = accuracies)
 ggplot(wrap, mapping = aes(x = Clusters, y = V.measure)) + geom_point() + geom_line() + theme_light()
+ggplot(wrap, mapping = aes(x = Clusters, y = Accuracy)) + geom_point() + geom_line() + theme_light()
 
-best_k <- which.max(vmeasures) + 1
+
+best_k <- which.max(accuracies) + 1
 best_k
 
-tree <- cutree(cluster_h, best_k)
-predict(plots_acsa2, tree)
+best <- cutree(cluster_h, best_k)
+plots_acsa2$cluster <- best
 
+tree <- rpart(data = plots_acsa2, formula = form, method = "class", 
+              parms = list(split = "information"),
+              control = rpart.control(cp = 0, minbucket = 1))
+
+rpart.plot(tree)
 #Hence, we find that for silver maples, cutting the Ward cluster at k = 5 is the best choice for validity (with max clusters equal to 20)
 #With max clusters at 100, the best choice is 94, which is obviously far too many
 
